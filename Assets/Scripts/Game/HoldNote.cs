3fefc1e4245b543ea8fc220fb9cc5a9276c97dc4
rgmoreno <garcia.moreno.ruben@gmail.com>
2014-11-12 01:37:15 +0000

    Added some graphics

---
+++ Assets/Scripts/Game/HoldNote.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoldNote : MonoBehaviour
+{
+	private const int size = 100;
+
+	public Transform spriteStart;
+	public Transform spriteMiddle;
+	public Transform spriteEnd;
+
+	void Start()
+	{
+		init (5);
+	}
+
+	public void init(float time)
+	{
+		spriteStart.localPosition = new Vector3(0, spriteStart.localPosition.y, spriteStart.localPosition.z);
+		spriteEnd.localPosition = new Vector3(size * (time + 1), spriteStart.localPosition.y, spriteStart.localPosition.z);
+
+		spriteMiddle.localScale = new Vector3(time, 1.0f, 1.0f);
+		spriteMiddle.localPosition = new Vector3(((time + 1) / 2) * size, spriteStart.localPosition.y, spriteStart.localPosition.z);
+
+	}
+}
---
+++ Assets/Scripts/Game/Note.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Note : MonoBehaviour
+{
+	public SpriteRenderer buttonSprite;
+
+	public void init()
+	{
+
+	}
+}
